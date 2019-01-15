adfeadde999e50a6491ad3a97100c51d28b66afc
张光普 <zhanggp@gohealth.com.cn>
2019-01-15 18:44:05 +0800

    合并

--- src/pages/testRsa/index.vue
+++ src/pages/testRsa/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="fcup"></div>  
+        <div class="fcup">大师傅</div>  
     </div>
 </template>
 <script>
@@ -9,135 +9,15 @@ import { ext } from "@/plugins/encrypt.js";
 export default {
     data() {
         return {
-            normalStr: "Hello world",
-            after: "",
-            publicKey: `-----BEGIN PUBLIC KEY-----
-            MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC/GTvHeZv0+WAQR5dwt02TWbON
-            gZqflwkcHT3xnY9cFYi0KiOsc5elZ2Ie6SM60RNBrDid19chCap682kxRy94vQzK
-            yfwhKLJz5gF3vpJS3q+QcvbSapRy/1ln54kqaw3KZNC05kDwO+dfAmHTeQ95rIZV
-            a0bDnxmYnVlvb0zOgwIDAQAB
-            -----END PUBLIC KEY-----
-            `,
-            privateKey: `-----BEGIN PRIVATE KEY-----
-            MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAL8ZO8d5m/T5YBBH
-            l3C3TZNZs42Bmp+XCRwdPfGdj1wViLQqI6xzl6VnYh7pIzrRE0GsOJ3X1yEJqnrz
-            aTFHL3i9DMrJ/CEosnPmAXe+klLer5By9tJqlHL/WWfniSprDcpk0LTmQPA7518C
-            YdN5D3mshlVrRsOfGZidWW9vTM6DAgMBAAECgYAGlYX7v3LiI91tcioDli00EHkh
-            PP/o07laoJ9LvdP8pjlJi9Ii1PC6oC8/RWUTV7TEJHzm41LkLoMTLWPIcN1vyiUT
-            u/54nH2GsQ8X77z91CgyBwEubo4RKd6E421OoIAJ5F80mQvJf471z+6C4URYVWY2
-            9tIEEtLccKxHm/wO8QJBAPvMGlqkWpifPF8iAcDtlpQom0JLdR4D+oDrH5qat2GL
-            0oz3/swV4EWFmftd1GgPdGkyoo18DxO1OizlXwoVEVUCQQDCScXazHugYqwCecOr
-            6JUo9y2Dv9X1qav5I16HLxEacDy6eK+5h4OtxSg+FDr0EGRMycs86llLy9md8sjw
-            esB3AkEAnGMn+yQZILHFzVBRxoHKBDs0MviqxPmznF5sQpnVS8CMBQICPr9Xi9Zu
-            zLB6PCddJflM/JMQyizE9R2A6cIsAQJAPOCOzVMbPZMNYK00oY38Bt0Edu7KJPKo
-            PyVU/jjCPC2E6czdCt4qZW2MgX8nRchJFUJ5mzm3Byf1pScozSk5WwJARfzCejsv
-            FBvjUEVpQ2Q9Gm0pGJlacEWXoxDtYSqMbgUdWQHTFvqUAI+owCxgyPk7C2hKttzS
-            W6KhxNieBuwHWA==
-            -----END PRIVATE KEY-----
-            `
+            
         };
     },
     mounted() {
-        // $.fcup({
-        //     updom: ".fcup", //上传控件的位置dom
-        //     //upid: 'upid',//上传的文件表单id，有默认
-        //     shardsize: "2", //切片大小,(单次上传最大值)单位M，默认2M
-        //     upmaxsize: "1024", //上传文件大小,单位M，不设置不限制
-        //     upstr: "上传文件", //按钮文字
-        //     uploading: "上传中...", //上传中的提示文字
-        //     upfinished: "上传完成", //上传完成后的提示文字
-        //     upurl: "./php/file.php", //文件上传接口 node接口:http://127.0.0.1:8888/upload
-        //     //uptype: 'mp4',//上传类型检测,用,号分割
-        //     errmaxup: "上传文件过大", //检测文件是否超出设置上传大小
-        //     errtype: "请上传mp4文件", //不支持类型的提示文字
-        //     //接口返回结果回调
-        //     upcallback: function(result) {
-        //         console.log(result);
-        //         /*
-		//  if (result !== 'success') {
-		// 	$.fcupStop('出现错误');//终止运行,并且在按钮上显示内容
-		//  }	
-        //  */
-        //     }
-        // })
-        /* base64 编码解码 */
-        // var str = 'Hello World';
-        // var encoded = window.Base64.encode(str);
-        // console.log('encoded',encoded);
-        // var decoded = Base64.decode(encoded);
-        // console.log(decoded);
-        // var base64 = new this.base64();
-        // var word = window.btoa(window.encodeURIComponent(str));
-        // console.log('加密后',word);
-        // var word = window.decodeURI(atob(word));
-        // console.log('解密后',word);
-        // var word = 'hello,您好';
-        // var key = CryptoJS.enc.Utf8.parse("中电国康医到服务");
-        // // function Encrypt(word){
-        // var srcs = CryptoJS.enc.Utf8.parse(word);
-        // var encrypted = CryptoJS.AES.encrypt(srcs, key, { mode:CryptoJS.mode.ECB,padding: CryptoJS.pad.Pkcs7});
-        // var word = encrypted.toString();
-        // console.log('加密后 ',word);
-        // var word = '5CngaJ+XcuQZC0yySiOeeg==';
-        // // }
-
-        // // function Decrypt(word){
-        // var encryptedHexStr = CryptoJS.enc.Hex.parse(word);
-        // var srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
-        // var decrypt = CryptoJS.AES.decrypt(word, key, { mode:CryptoJS.mode.ECB,padding: CryptoJS.pad.Pkcs7});
-        // var decryptedStr = decrypt.toString(CryptoJS.enc.Utf8).toString();
-        // console.log('解密前 ',word);
-        // console.log('解密后 ',decryptedStr);
-        // // }
-
-        // var source = "ABC";
-        // var target = Encrypt(source);
-        // console.log(target);
-        // console.log(Decrypt(target));
-
-        // var data = "Test String";
-        // var key  = CryptoJS.enc.Latin1.parse('中电国康医到服务');
-        // var iv   = CryptoJS.enc.Latin1.parse('中电国康医到服务');
-
-        // //加密
-        // var encrypted = CryptoJS.AES.encrypt(data,key,{iv:iv,mode:CryptoJS.mode.CBC,padding:CryptoJS.pad.ZeroPadding});
-        // console.log('encrypted',encrypted);
-        // console.log('ciphertext',encrypted.ciphertext);
-
-        // //解密
-        // var decrypted = CryptoJS.AES.decrypt(encrypted,key,{iv:iv,padding:CryptoJS.pad.ZeroPadding});
-        // console.log('decrypted',decrypted.toString(CryptoJS.enc.Utf8));
-        // var word = this.aesUtils.encrypt('hello,您好','中电国康医到服务');
-        // console.log('后台加密','5CngaJ+XcuQZC0yySiOeeg==');
-        // console.log('前台加密',word);
-        // var after = this.aesUtils.decrypt(word,'中电国康医到服务');
-        // var after = this.aesUtils.aesDecrypt(word,'中电国康医到服务');
-        // console.log('解密后',after);
-        // let a = ext.en(this.normalStr)
-        // let b = ext.de(a)
-        // console.log(a,'加密后的结果')
-        // console.log(b,'解密后的结果')
-        // console.log(this.RSAUtils);
-        // let that = this;
-        // // 实例化一个JSEncrypt对象
-        // let jse = new that.$jsEncrypt();
-        // jse.setPublicKey(this.publicKey);
-        // // 设置需要加密的字符串
-        // let encrypted = jse.encrypt('Hello, world');
-        // console.log(encrypted);
-        // jse.setPrivateKey(this.privateKey);
-        // // // 解密加密过的字符串
-        // let decrypted = jse.decrypt(encrypted)
-        // console.log(decrypted);
-        // let str = 'Wo Shi Shei Wo Shi Cheng Long';
-        // let regexp = new RegExp('[A-Z]','g');
-        // let res = str.match(regexp);
-        // console.log(res);
-        // let py = this.$py({value:'我是中国人',type:'firstUp'});
-        // let regexp = new RegExp('[A-Z]','g');
-        // let res = py.match(regexp);
-        // console.log('找出来的大写字母数组',res);
-        // console.log('找出来的大写字母数组转换成都好分割字符串',res.join());
+        var word = this.aesUtils.encrypt('hello,您好','中电国康医到服务');
+        console.log('后台加密','5CngaJ+XcuQZC0yySiOeeg==');
+        console.log('前台加密',word);
+        var after = this.aesUtils.decrypt(word,'中电国康医到服务');
+        console.log('解密后',after);
     }
 };
 </script>
--- src/plugins/aes-utils.js
+++ src/plugins/aes-utils.js
@@ -1,7 +1,8 @@
 import CryptoJS from 'crypto-js'
 
 export default {
-
+    KEY:CryptoJS.enc.Utf8.parse("中电国康医到服务"),
+    IV:CryptoJS.enc.Utf8.parse("中电国康医到服务"),
     /**
     
     * @param {*需要加密的字符串 注：对象转化为json字符串再加密} word
@@ -9,25 +10,57 @@ export default {
     * @param {*aes加密需要的key值，这个key值后端同学会告诉你} keyStr
     
     */
-    encrypt(word, keyStr) { // 加密
+    // encrypt(word, keyStr) { // 加密
 
-        var key = CryptoJS.enc.Utf8.parse(keyStr)
-        console.log('key= ',key);
-        var srcs = CryptoJS.enc.Utf8.parse(word)
-        var encrypted = CryptoJS.AES.encrypt(srcs, key, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 }) // 加密模式为ECB，补码方式为PKCS5Padding（也就是PKCS7）
-        // var encrypted = CryptoJS.AES.encrypt('5CngaJ+XcuQZC0yySiOeeg==', '中电国康医到服务', { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs5 }) // 加密模式为ECB，补码方式为PKCS5Padding（也就是PKCS7）
-        
-        return encrypted.toString()
-        
+    //     var key = CryptoJS.enc.Utf8.parse(keyStr)
+    //     console.log('key= ',key);
+    //     var srcs = CryptoJS.enc.Utf8.parse(word)
+    //     var encrypted = CryptoJS.AES.encrypt(srcs, key, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 }) // 加密模式为ECB，补码方式为PKCS5Padding（也就是PKCS7）
+    //     // var encrypted = CryptoJS.AES.encrypt('5CngaJ+XcuQZC0yySiOeeg==', '中电国康医到服务', { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs5 }) // 加密模式为ECB，补码方式为PKCS5Padding（也就是PKCS7）
+
+    //     return encrypted.toString()
+
+    // },
+    encrypt(word, keyStr, ivStr) {
+        let key = this.KEY
+        let iv = this.IV
+
+        if (keyStr) {
+            key = CryptoJS.enc.Utf8.parse(keyStr);
+            iv = CryptoJS.enc.Utf8.parse(ivStr);
+        }
+
+        let srcs = CryptoJS.enc.Utf8.parse(word);
+        var encrypted = CryptoJS.AES.encrypt(srcs, key, {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.ZeroPadding
+        });
+        // console.log("-=-=-=-", encrypted.ciphertext)
+        return CryptoJS.enc.Base64.stringify(encrypted.ciphertext);
+
     },
-    
-    decrypt(word, keyStr) { // 解密
-        
-        var key = CryptoJS.enc.Utf8.parse(keyStr)
-        var decrypt = CryptoJS.AES.decrypt(word, key, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 })
-        console.log('decrypt',decrypt);
-        return CryptoJS.enc.Utf8.stringify(decrypt).toString()
 
+    decrypt(word, keyStr, ivStr) {
+        let key = this.KEY
+        let iv = this.IV
+
+        if (keyStr) {
+            key = CryptoJS.enc.Utf8.parse(keyStr);
+            iv = CryptoJS.enc.Utf8.parse(ivStr);
+        }
+
+        let base64 = CryptoJS.enc.Base64.parse(word);
+        let src = CryptoJS.enc.Base64.stringify(base64);
+
+        var decrypt = CryptoJS.AES.decrypt(src, key, {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.ZeroPadding
+        });
+
+        var decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+        return decryptedStr.toString();
     }
 
 }
