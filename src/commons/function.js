0623deb41c970a7a62e502198c62b55e41b6e715
张光普 <zhanggp@gohealth.com.cn>
2019-01-21 16:02:17 +0800

    修改动态菜单刷新问题

--- src/commons/function.js
+++ src/commons/function.js
@@ -1,6 +1,7 @@
 import store from '@/store';
 import cookie from '@/utils/cookie.js';
 import aesUtils from '@/plugins/aes-utils.js';
+//生成AddDayCount天之前/之后的年月日
 export function GetDate(AddDayCount=0) {
     var dd = new Date();
     dd.setDate(dd.getDate() + AddDayCount);//获取AddDayCount天后的日期
@@ -15,6 +16,7 @@ export function GetDate(AddDayCount=0) {
     }
     return y + "-" + m + "-" + d;
 }
+//上传接口返回值解密
 export function uploadFileDecrypt(res){
     let salt = store.state.salt;
     let iv = store.state.iv;
@@ -24,3 +26,11 @@ export function uploadFileDecrypt(res){
     }
     return res;
 }
+//查询cha在str中第num个的位置,num从0开始
+export function findStr(str,cha,num){
+    var x=str.indexOf(cha);
+    for(var i=0;i<num;i++){
+        x=str.indexOf(cha,x+1);
+    }
+    return x;
+    }
