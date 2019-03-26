c8a1040674706a41890c76f4239884919c238398
张光普 <zhanggp@gohealth.com.cn>
2019-03-26 17:18:09 +0800

    取消netease登陆

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -126,8 +126,8 @@ export default {
     },
     updated(){
         // 提交sdk连接请求
-        this.$store.dispatch("connect");
-        this.$store.dispatch("updateRefreshState");
+        // this.$store.dispatch("connect");
+        // this.$store.dispatch("updateRefreshState");
     },
     computed: {
         leftMenuLists() {
