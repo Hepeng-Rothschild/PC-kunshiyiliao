5b9e51d54e05f3d3acd4630eb4d058203ceff30e
张光普 <zhanggp@gohealth.com.cn>
2019-04-29 15:24:51 +0800

    修改https.js报错处理，testapi向productionapi复制一份

--- config/index.js
+++ config/index.js
@@ -24,8 +24,8 @@ module.exports = {
 
     // Various Dev Server settings
     // host: '172.18.1.250', // can be overwritten by process.env.HOST
-    // host:'localhost',
-    host:"192.168.43.46",
+    host:'localhost',
+    // host:"192.168.43.46",
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
